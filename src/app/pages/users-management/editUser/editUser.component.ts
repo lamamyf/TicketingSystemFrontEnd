97e67305aceec7d9d7bc0c7619a6419d8b17ec19
fawziahAlaqil <85229759+fawziahAlaqil@users.noreply.github.com>
2021-08-29 14:43:42 +0300

    Update editUser

--- src/app/pages/users-management/editUser/editUser.component.ts
+++ src/app/pages/users-management/editUser/editUser.component.ts
@@ -7,6 +7,8 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {Location} from '@angular/common';
 import {RouterExtService} from '../../../services/RouterExtService.service';
+import { ConfirmationDialogComponent } from '../../confirmation-dialog/confirmation-dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-edituser',
@@ -23,6 +25,7 @@ export class EditUserComponent implements OnInit, OnDestroy {
   hasError: boolean;
   previousUrl: string;
   isActive;
+  dialogRef: MatDialogRef<ConfirmationDialogComponent>;
   constructor(private userService: AuthService,
               private fb: FormBuilder,
               private apiService: ApiService,
@@ -31,7 +34,9 @@ export class EditUserComponent implements OnInit, OnDestroy {
               private cdr: ChangeDetectorRef,
               private location: Location,
               private routerExtService: RouterExtService,
-              private activatedRoute: ActivatedRoute
+              private activatedRoute: ActivatedRoute,
+              public dialog: MatDialog,
+             
   ) {
       this.isLoading$ = this.userService.isLoadingSubject.asObservable();
       this.activatedRoute.queryParams.subscribe(params => {
@@ -39,9 +44,6 @@ export class EditUserComponent implements OnInit, OnDestroy {
           this.isActive = params.isActive;
       });
   }
-
-
-
 
     ngOnInit(): void {
     this.hasError = true;
@@ -56,24 +58,21 @@ export class EditUserComponent implements OnInit, OnDestroy {
       this.hasError = false;
       this.formGroup = this.fb.group({
       id: [this.userID],
-      currentPassword: ['', Validators.required],
-      password: ['', Validators.compose([
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.compose([
           Validators.required,
-          Validators.minLength(8),
-          Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&].{3,}')
-      ])],
-      confirmPassword: ['', Validators.required],
-      active: [this.isActive],
-      }, {
-      validator: ConfirmPasswordValidator.MatchPassword
-      });
+          Validators.minLength(3), ])],
+      gender:['', Validators.required],
+
+     
+      }, );
   }
 
   save() {
       const saveSubscr = this.apiService
         .editUser(this.formGroup.value).subscribe(res => {
             if (!res.body) {
-             this.router.navigate(['/usersManagement']).then(r =>
+             this.router.navigate(['/dashboard']).then(r =>
                  this.snackBar.open('تم تعديل المستخدم بنجاح', '', {
                      duration: 2000
                  })
@@ -96,6 +95,62 @@ export class EditUserComponent implements OnInit, OnDestroy {
       }
 
   }
+  deleteUser(){
+
+    this.dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      disableClose: false,
+      width: '500px',
+      height: '170px'
+  });
+  this.dialogRef.componentInstance.confirmMessage = 'هل أنت متآكد من حذف الحساب';
+  this.dialogRef.afterClosed().subscribe(result => {
+
+    this.dialogRef = null;
+  });
+
+  }
+
+//   deleteUser(user: UserModel) {
+//     this.dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+//         disableClose: false,
+//         width: '500px',
+//         height: '170px'
+//     });
+//     this.dialogRef.componentInstance.confirmMessage = 'هل آنت متآكد من حذف الحساب ' + user.firstName+" "+user.lastName + ' ؟';
+//     this.dialogRef.afterClosed().subscribe(result => {
+//         if (result) {
+//             this.apiService
+//                 .deleteUser(user.id).pipe()
+//                 .subscribe((data: Response) => {
+//                     const resStr = JSON.stringify(data);
+//                     const resJSON = JSON.parse(resStr);
+//                     if (resJSON.statusCodeValue === 200){
+//                         this.loadUsers();
+//                         this.snackBar.open('تم حذف المستخدم بنجاح', '', {
+//                             duration: 2000,
+//                         });
+//                     }
+//                 });
+//         }
+//         this.dialogRef = null;
+//     });
+// }
+
+// getUser() : UserModel []  {
+
+// let user: UserModel[] = [
+
+//     {
+//         id: 1,
+//         firstName: "fawziah",
+//         lastName: "alaqil",
+//         password: "faerthnjj888",
+//         email: "fawziah514@gmail.com",
+//         avatar: "./assets/media/svg/avatars/default.jpg",
+//         gender: "انثى",
+
+//   }];
+// return user}
 
 }
 
